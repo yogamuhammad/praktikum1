81b1bb2df929012e54074154ac21ef5dd336bcab
Yoga Muhammad Rojuludin <yogamuhamad10@gmail.com>
2021-10-26 02:52:50 +0700

    menambahkan file

---
+++ latihan1.py
@@ -0,0 +1,20 @@
+print ("hello")
+print ("saya sedang belajar python")
+
+a = 8
+b = 6
+print ("variable a=", a)
+print ("variable b=", b)
+print ("hasil penjumlahan a+b=", a+b)
+
+a=input ("masukan nilai a:")
+b=input ("masukan nilai b:")
+print ("variable a=",a)
+print ("variable b=",b)
+print ("hasil pergabungan {1}&{0}=%d". format (a,b) %(a+b))
+
+#konversi nilai variable
+a=int(a)
+b=int(b)
+print ("hasil penjumlahan {1}+{0}=%d". format (a,b) %(a+b))
+print ("hasil pembagian {1}/{0}=%d". format (a,b) %(a/b))
